3217639401268979dcc6f9d814ea3d39ba61e349
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-04-28 00:11:59 +0500

    User more than one useState Hook

--- src/App.js
+++ src/App.js
@@ -8,21 +8,27 @@ function countInitial(){
 
 function App() {
 
-  const [count, setCount] = useState(()=>countInitial()) //run one time
+  const [count, setCount] = useState(4) //run one time
+  const [theme, setTheme] = useState("red")
   
+
   function increment(){
-    setCount(nextCount => nextCount + 1) //useful because it each time take updated value but no same value when this function runs first time
+    setCount(prevCount => prevCount + 1)
+    setTheme("blue")
   }
 
   function decrement(){
-    setCount(prevCount => prevCount - 1)
+    setCount(nextCount => nextCount + 1) //useful because it each time take updated value but no same value when this function runs first time
+    setTheme("red")
   }
 
   return (
     <>
-      <button onClick={increment}>+</button>
-      <span>{count}</span> 
       <button onClick={decrement}>-</button>
+      <span>{count}{theme}</span> 
+      <button onClick={increment}>+</button>
+      
+      
     </>
   );
 }
